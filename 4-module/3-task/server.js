e5f10cb65a2bb78411116508ecb913fa1cd292c3
Ivan <ivfisunov@gmail.com>
2019-09-06 23:44:39 +0300

    add solution for module 4 task 3

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.end('Subdirs not supported');
+        return;
+      }
+      fs.exists(filepath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.end('File not found');
+          return;
+        } else {
+          fs.unlink(filepath, (err) => {
+            if (err) {
+              console.error(err);
+            }
+            res.statusCode = 200;
+            res.end('File deleted');
+          });
+        }
+      });
       break;
 
     default:
