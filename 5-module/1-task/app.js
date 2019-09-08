e9ebf0ddce3b9b42d31ad4e9338b5dac377e16cb
Ivan <ivfisunov@gmail.com>
2019-09-08 16:52:08 +0300

    sol 5mod 1tsk

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,16 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
+const serve = require('koa-static');
 
-app.use(require('koa-static')('public'));
+app.use(serve('public'));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get('/subscribe', async (ctx) => {
+  ctx.body = await new Promise((resolve) => {
+    app.on('message', (message) => {
+      if (message) {
+        resolve(message);
+      }
+    });
+  });
+  app.on('close', () => resolve());
+  ctx.response.status = 200;
 });
 
-router.post('/publish', async (ctx, next) => {
+
+router.post('/publish', async (ctx) => {
+  // console.log(ctx.request.body.message);
+  ctx.response.status = 201;
+  ctx.app.emit('message', ctx.request.body.message);
 });
 
 app.use(router.routes());
