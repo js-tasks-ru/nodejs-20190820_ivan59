899ac0c40b3b504ecd106a9f0b156159596c58a2
Ivan <ivfisunov@gmail.com>
2019-09-11 22:57:56 +0300

    sol for 6-2&3

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,7 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({});
+  // console.log(categories);
+  ctx.body = {categories: categories};
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,45 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {products: []};
+
+  if (ctx.query && ctx.query.subcategory) {
+    if (mongoose.Types.ObjectId.isValid(ctx.query.subcategory)) {
+      const products = await Product.find({ subcategory: ctx.query.subcategory })
+      ctx.status = 200;
+      ctx.body = { products };
+    } else {
+      ctx.status = 404;
+      ctx.body = { products: [] };
+    }
+  } else {
+    return next();
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {products: []};
+  console.log('2');
+  const products = await Product.find({});
+  ctx.statusCode = 200;
+  ctx.body = { products };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {product: {}};
+  const id = ctx.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    ctx.status = 404;
+    return;
+  }
+
+  ctx.status = 200;
+  ctx.body = { product };
 };
 
